81a0edf41742d6eb88cc05c6f39d3d45c223898b
Aleksey Yeletsky <a.eletsky@gmail.com>
2011-09-23 18:57:20 +0000

    gh-6 заменить тег menu на nav

--- riddles_collection/settings_local.py
+++ riddles_collection/settings_local.py
@@ -7,8 +7,8 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'riddles',
-        'USER': 'root',
-        'PASSWORD': '',
+        'USER': 'riddles',
+        'PASSWORD': 'riddles',
         'HOST': '',
         'PORT': '',
     }
