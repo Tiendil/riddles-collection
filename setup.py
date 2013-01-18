d0ab18c7453e19da35689d77f2d04a20ead266cd
Tiendil <a.eletsky@gmail.com>
2013-01-18 12:45:59 +0300

    setup config

---
+++ setup.py
@@ -0,0 +1,15 @@
+# coding: utf-8
+import setuptools
+
+setuptools.setup(
+    name = 'Riddles',
+    version = '0.1.0',
+    author = 'Aleksey Yeletsky',
+    author_email = 'a.eletsky@gmail.com',
+    packages = setuptools.find_packages(),
+    url = 'https://github.com/Tiendil/riddles-collection',
+    license = 'LICENSE',
+    description = "my hobby project — collection of russian riddles",
+    long_description = open('README').read(),
+    include_package_data = True # setuptools-git MUST be installed
+)
